60019fd97b554f79fbae5114c77da0cb8f2e6850
Mateus <mateuswagnitzdev@gmail.com>
2021-03-14 18:24:25 -0300

    Done

---
+++ README.md
@@ -0,0 +1,2 @@
+# TODO
+A Todo list made with .net core and mySQL;
---
+++ ToDoList/ToDoList/Models/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TaskDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;userid=root;password=1234567;database=todo;");
+        }
+    }
+}
